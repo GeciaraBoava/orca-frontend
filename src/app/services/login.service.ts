d43463da59f7c460f8242803cd66048dab8012d5
geciara <geciara.boava@rpisolucoes.com.br>
2025-11-07 14:13:45 -0300

    🎉 :: [FistCommit] :: Tela de login estruturada

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LoginResponse} from '../types/login-response.type';
+import {tap} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(username: string, password: string) {
+    return this.httpClient.post<LoginResponse>("/login", {username, password}).pipe(
+      tap((value) => { //tasp - retorna um valor síncrono
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.username)
+      }
+    ))
+  }
+
+}
